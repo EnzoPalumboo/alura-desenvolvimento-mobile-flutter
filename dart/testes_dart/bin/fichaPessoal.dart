68c96a8bb25474b03d14dbfa45768bbfc9c21d4e
Enzo Martins Palumbo Ribeiro <enzopalumbo64@gmail.com>
2026-04-09 10:45:19 -0300

    Ficha pessoal

---
+++ dart/testes_dart/bin/fichaPessoal.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nome = "Enzo";
+  String CPF = "123.456.789-00";
+  int idade = 20;
+  double altura = 1.78;
+
+  print("Eu sou o $nome");
+  print("Meu CPF é: $CPF");
+  print("Minha idade é: $idade");
+  print("Eu tenho: $altura de altura");
+}
