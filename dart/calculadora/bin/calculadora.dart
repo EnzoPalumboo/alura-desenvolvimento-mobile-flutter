74edce3e0f0a5eeaf55e8a222938ed5f85efd272
Enzo Martins Palumbo Ribeiro <enzopalumbo64@gmail.com>
2026-04-14 13:23:43 -0300

    Adicionando todas operações

--- dart/calculadora/bin/calculadora.dart
+++ dart/calculadora/bin/calculadora.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 void main() {
   double numeroUm = double.parse(stdin.readLineSync()!);
   double numeroDois = double.parse(stdin.readLineSync()!);
+  String operacao = stdin.readLineSync()!;
 
   void soma() {
     print(numeroUm + numeroDois);
@@ -19,6 +20,28 @@ void main() {
   void multiplacacao() {
     print(numeroUm * numeroDois);
   }
+  if (operacao == 'soma') {
+    soma();
+  } else if (operacao == 'subtracao') {
+    subtracao();
+  } else if (operacao == 'divisao') {
+    divisao();
+  } else if (operacao == 'multiplicacao') {
+    multiplacacao();
+  }
 
-  soma();
+  switch (operacao) {
+    case 'soma':
+      soma();
+      break;
+    case 'subtracao':
+      subtracao();
+      break;
+    case 'divisao':
+      divisao();
+      break;
+    case 'multiplicacao':
+      multiplacacao();
+      break;
+  }
 }
