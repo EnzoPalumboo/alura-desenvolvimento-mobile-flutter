701c6b86491150947cf43f7bc2ad52c971c0b1ec
Enzo Martins Palumbo Ribeiro <enzopalumbo64@gmail.com>
2026-04-24 15:48:44 -0300

    Otimização do Codigo

--- dart/anybank/bin/anybank.dart
+++ dart/anybank/bin/anybank.dart
@@ -1,4 +1,3 @@
-
 import 'package:anybank/conta.dart';
 
 void main() {
@@ -8,21 +7,10 @@ void main() {
   List<Conta> contas = [contaMatheus, contaRoberta];
 
   for (Conta conta in contas) {
-    print(conta.titular);
-    print(conta._saldo);
+    conta.imprimeSaldo();
   }
 
-  contaRoberta._saldo = 5000;
-
-  print(contaRoberta._saldo);
-
+  contaRoberta.receber(3000);
   contaMatheus.receber(500);
-
-  print(contaMatheus.titular);
-  print(contaMatheus._saldo);
-
   contaMatheus.enviar(200);
-
-  print(contaMatheus.titular);
-  print(contaMatheus._saldo);
 }
--- dart/anybank/lib/conta.dart
+++ dart/anybank/lib/conta.dart
@@ -7,14 +7,18 @@ class Conta {
   // Receber dinheiro
   void receber(double valor) {
     _saldo += valor;
+    imprimeSaldo();
   }
 
   // Enviar dinheiro
   void enviar(double valor) {
     if (_saldo >= valor) {
       _saldo -= valor;
-    } else {
-      print("Saldo insuficiente para enviar o valor de $valor");
+      imprimeSaldo();
     }
+  }
+
+  void imprimeSaldo() {
+    print("O saldo atual de $titular, é de R\$$_saldo");
   }
 }
